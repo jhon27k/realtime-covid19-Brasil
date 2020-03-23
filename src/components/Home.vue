80f7af1fd1922a79039377830ee14153747d788b
Jonathas <jonathas.ec14@gmail.com>
2020-03-23 12:46:50 -0400

    requisições finalizadas

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,21 +6,140 @@
           <v-card color="primary" dark>
             <v-card-text>
               Carregando Dados...
-              <v-progress-linear
-                indeterminate
-                color="white"
-                class="mb-0"
-              ></v-progress-linear>
+              <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
             </v-card-text>
           </v-card>
         </v-dialog>
       </div>
-      <div class="container-fluid" v-if="!dialog">
-        <div class="row" id="dados1">
-          <div class="col-md-4 ">1</div>
-          <div class="col-md-4">2</div>
-          <div class="col-md-4">3<i class="fas fa-address-book"></i></div>
+      <div class="container-fluid text-center" v-if="!dialog">
+        <div class="titulo">
+          <h1 class="txt-titulo">INFORMAÇÕES EM TEMPO REAL CORONA VÍRUS</h1>
         </div>
+        <div class="titulo">
+          <div class="selecionar-pais">
+            <form class="form-group">
+              <v-row align="center">
+                <v-col cols="12">
+                  <select class="form-control" v-model="paisSelecionado" @change="BuscarDados(paisSelecionado)" style="width:100%">
+                    <option :value="paisSelecionado">Outros Países</option>
+                    <option v-for="pais in paises" :value="pais" :key="pais">{{pais}}</option>
+                  </select>
+                </v-col>
+              </v-row>
+              </form>
+          </div>
+        </div>
+        <div class="row shadow p-3 mb-5 bg-white rounded">
+                    <div class="col-md-4">
+            <div class="container card shadow p-3 mb-5 bg-white rounded">
+              <div class="card-title mb-2">
+                <h2>PAÍS</h2>
+              </div>
+              <div class="card-body">
+                <h2 class="card-h2 mb-5" style="text-transform: uppercase">{{paisSelecionado}}</h2>
+              </div>
+
+              <!-- <div class="card img-fluid">
+                <img
+                  class="card-img-top"
+                  src="../assets/1.jpg"
+                  alt="Card image"
+                  style="width:100%;"
+                />
+              </div>-->
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="container card shadow p-3 mb-5 bg-white rounded">
+              <div class="card-title mb-2">
+                <h2>CASOS CONFIRMADOS</h2>
+              </div>
+              <div class="card-body">
+                <h2 class="card-h2 mb-5">{{dados.cases}}</h2>
+              </div>
+
+              <!-- <div class="card img-fluid">
+                <img
+                  class="card-img-top"
+                  src="../assets/1.jpg"
+                  alt="Card image"
+                  style="width:100%;"
+                />
+              </div>-->
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="container card shadow p-3 mb-5 bg-white rounded">
+              <div class="card-title mb-2">
+                <h2>CASOS HOJE</h2>
+              </div>
+              <div class="card-body">
+                <h2 class="card-h2 mb-5">{{dados.todayCases}}</h2>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="container card shadow p-3 mb-5 bg-white rounded">
+              <div class="card-title mb-2">
+                <h2>MORTES</h2>
+              </div>
+              <div class="card-body">
+                <h2 class="card-h2 mb-5">{{dados.deaths}}</h2>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="container card shadow p-3 mb-5 bg-white rounded">
+              <div class="card-title mb-2">
+                <h2>MORTES HOJE</h2>
+              </div>
+              <div class="card-body">
+                <h2 class="card-h2 mb-5">{{dados.todayDeaths}}</h2>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="container card shadow p-3 mb-5 bg-white rounded">
+              <div class="card-title mb-2">
+                <h2>RECUPERADOS</h2>
+              </div>
+              <div class="card-body">
+                <h2 class="card-h2 mb-5">{{dados.recovered}}</h2>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="container card shadow p-3 mb-5 bg-white rounded">
+              <div class="card-title mb-2">
+                <h2>ATIVOS</h2>
+              </div>
+              <div class="card-body">
+                <h2 class="card-h2 mb-5">{{dados.active}}</h2>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="container card shadow p-3 mb-5 bg-white rounded">
+              <div class="card-title mb-2">
+                <h2>CASOS CRÍTICOS</h2>
+              </div>
+              <div class="card-body">
+                <h2 class="card-h2 mb-5">{{dados.critical}}</h2>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="container card shadow p-3 mb-5 bg-white rounded">
+              <div class="card-title mb-2">
+                <h2>CASOS A CADA 1M</h2>
+              </div>
+              <div class="card-body">
+                <h2 class="card-h2 mb-5">{{dados.casesPerOneMillion}}</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- FIM CARDS -->
       </div>
     </template>
   </v-container>
@@ -37,12 +156,17 @@ export default {
   },
   data() {
     return {
-      dados: "",
-      dialog: true
+      dados: [],
+      dialog: true,
+      i: 0,
+      paises: [],
+      paisSelecionado: ""
     };
   },
   mounted() {
-    this.BuscarDados();
+    this.paisSelecionado = "Brazil";
+    this.BuscarDados(this.paisSelecionado);
+    this.BuscarPaises();
   },
   watch: {
     dialog(val) {
@@ -51,14 +175,25 @@ export default {
     }
   },
   methods: {
-    BuscarDados() {
-      // this.Msg("load", "success");
+    BuscarDados(paisSelecionado) {
       this.axios
-        .get("/brazil")
+        .get("/" + paisSelecionado)
         .then(response => {
           this.dados = response.data;
-          console.log(this.dados);
           setTimeout(() => (this.dialog = false), 2000);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    BuscarPaises() {
+      this.axios
+        .get("/")
+        .then(response => {
+          var dado = response.data;
+          dado.forEach(element => {
+            this.paises.push(element.country);
+          });
         })
         .catch(error => {
           console.log(error);
@@ -84,9 +219,32 @@ export default {
 </script>
 
 <style scoped>
-  #dados1{
-    display: flex;
-    align-content: center;
-  }
+#dados1 {
+  display: flex;
+  align-content: center;
+}
 
+.card {
+  background-color: #e2e8f0;
+  color: #718096;
+  font-size: 1.3rem;
+}
+.card-title {
+  background-color: #718096;
+  color: #e2e8f0;
+  margin: auto;
+  vertical-align: middle;
+  line-height: 60px;
+}
+card-body {
+  background-color: #fff5f5;
+  color: #e53e3e;
+}
+.titulo {
+  padding: 15px;
+  margin: 15px;
+  min-height: 50px;
+  font-size: 1.5rem;
+  background-color: #e2e8f0;
+}
 </style>
